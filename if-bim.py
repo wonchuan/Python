10c1106bc2eb019778a1ef12a77c9ea1f3e56e96
Chuan Wan <service@kiiyi.com>
2016-06-21 20:31:52 +0800

    a Bim if Program

---
+++ if-bim.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+h = input('输入你的身高:')
+w = input('输入你的体重:')
+height =float(h)
+weight =float(w)
+bmi= (weight/height) **2 *100
+print (bmi)
+if bmi < 18.5:
+    print('你的BMI 指数:%.2f,过轻' %bmi)
+elif bmi >= 18.5:
+    print('你的BMI 指数:%.2f,正常' %bmi)
+elif bmi >=25:
+    print('你的BMI 指数:%.2f,过胖' %bmi)
+elif bmi >28:
+    print('你的BMI 指数:%.2f,肥胖' %bmi)
+elif bmi >32:
+    print ('你的BMI 指数:%.2f,严重肥胖' %bmi)
+    
