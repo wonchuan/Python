8347815eaaa1600ff205d7dc4ca8eb70098e2322
Chuan Wan <service@kiiyi.com>
2016-06-21 20:45:03 +0800

    fix bmi cm to metre

--- if-bim.py
+++ if-bim.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-h = input('输入你的身高:')
+h = input('输入你的身高(cm):')
 w = input('输入你的体重:')
-height =float(h)
+height =float(h)/100 #转换成x米
 weight =float(w)
-bmi= (weight/height) **2 *100
+bmi= weight/(height*height)
+print (bmi)
 if bmi < 18.5:
     print('你的BMI 指数:%.2f,过轻' %bmi)
 elif bmi >= 18.5:
